2434630817e9eca931e2fdc42df493b9178a1596
Portada24 <123512224+Portada24@users.noreply.github.com>
2026-05-23 14:46:54 -0500

    fix url back

--- barberia-app/frontend/src/services/api.js
+++ barberia-app/frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'barber-a-urbanstyle-production.up.railway.app/api',
+  baseURL: 'https://barber-a-urbanstyle-production.up.railway.app/api',
   // baseURL: 'http://localhost:3001/api',
   headers: { 'Content-Type': 'application/json' },
 });
